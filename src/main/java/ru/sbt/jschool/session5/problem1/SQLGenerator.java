b211efe73ce284813d78a45586bfbb01e15fb01c
avryabov <antonrbv@gmail.com>
2018-04-02 12:03:47 +0300

    session5, problem1 remove reg exp

--- src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
@@ -1,10 +1,8 @@
 package ru.sbt.jschool.session5.problem1;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  */
@@ -16,119 +14,60 @@ public class SQLGenerator {
     private final String SELECT = "SELECT %s FROM %s WHERE %s";
 
     public <T> String insert(Class<T> clazz) {
-        Annotation[] ann = clazz.getAnnotations();
-        String tableName = annotateName(ann[0]);
-
-        LinkedList<Parameter> params = paramsList(clazz.getDeclaredFields());
-
-        return String.format(INSERT, tableName, paramsStr(params, AnnotType.All, ", ", ""),
-                paramsStr(params, AnnotType.Secret, ", ", ""));
+        String tableName = clazz.getAnnotation(Table.class).name();
+        Field[] fields = clazz.getDeclaredFields();
+        return String.format(INSERT, tableName, paramsStr(fields, new Class[]{Column.class, PrimaryKey.class}, true, ", ", ""),
+                paramsStr(fields, new Class[]{Column.class, PrimaryKey.class}, false, ", ", ""));
     }
 
     public <T> String update(Class<T> clazz) {
-        Annotation[] ann = clazz.getAnnotations();
-        String tableName = annotateName(ann[0]);
-
-        LinkedList<Parameter> params = paramsList(clazz.getDeclaredFields());
-
-        return String.format(UPDATE, tableName, paramsStr(params, AnnotType.Column, ", ", " = ?"),
-                paramsStr(params, AnnotType.PrimaryKey, " AND ", " = ?"));
+        String tableName = clazz.getAnnotation(Table.class).name();
+        Field[] fields = clazz.getDeclaredFields();
+        return String.format(UPDATE, tableName, paramsStr(fields, new Class[]{Column.class}, true, ", ", " = ?"),
+                paramsStr(fields, new Class[]{PrimaryKey.class}, true, " AND ", " = ?"));
     }
 
     public <T> String delete(Class<T> clazz) {
-        Annotation[] ann = clazz.getAnnotations();
-        String tableName = annotateName(ann[0]);
-
-        LinkedList<Parameter> params = paramsList(clazz.getDeclaredFields());
-
-        return String.format(DELETE, tableName, paramsStr(params, AnnotType.PrimaryKey, " AND ", " = ?"));
+        String tableName = clazz.getAnnotation(Table.class).name();
+        Field[] fields = clazz.getDeclaredFields();
+        return String.format(DELETE, tableName, paramsStr(fields, new Class[]{PrimaryKey.class}, true, " AND ", " = ?"));
     }
 
     public <T> String select(Class<T> clazz) {
-        Annotation[] ann = clazz.getAnnotations();
-        String tableName = annotateName(ann[0]);
-
-        LinkedList<Parameter> params = paramsList(clazz.getDeclaredFields());
-
-        return String.format(SELECT, paramsStr(params, AnnotType.Column, ", ", ""), tableName,
-                paramsStr(params, AnnotType.PrimaryKey, " AND ", " = ?"));
+        String tableName = clazz.getAnnotation(Table.class).name();
+        Field[] fields = clazz.getDeclaredFields();
+        return String.format(SELECT, paramsStr(fields, new Class[]{Column.class}, true, ", ", ""), tableName,
+                paramsStr(fields, new Class[]{PrimaryKey.class}, true, " AND ", " = ?"));
     }
 
-    private class Parameter {
-        AnnotType type;
-        String name;
-
-        public Parameter(AnnotType type, String name) {
-            this.type = type;
-            this.name = name;
+    private String paramsStr(Field[] fields, Class[] clazzes, boolean isName, String div, String add) {
+        List<String> list = new ArrayList<>();
+        for (Field field : fields) {
+            String name = field.getName().toLowerCase();
+            for (Class clazz : clazzes) {
+                if (field.isAnnotationPresent(clazz)) {
+                    if (isName) {
+                        String nameAnn = "";
+                        if (clazz == Column.class)
+                            nameAnn = field.getAnnotation(Column.class).name().toLowerCase();
+                        else if (clazz == PrimaryKey.class)
+                            nameAnn = field.getAnnotation(PrimaryKey.class).name().toLowerCase();
+                        if (nameAnn.equals(""))
+                            list.add(name);
+                        else
+                            list.add(nameAnn);
+                    } else
+                        list.add("?");
+                }
+            }
         }
-    }
-
-    private enum AnnotType {All, PrimaryKey, Column, Secret}
-
-    private LinkedList<Parameter> paramsList(Field[] fields) {
-        LinkedList<Parameter> list = new LinkedList<>();
-        for (Field f : fields) {
-            String name = f.getName();
-            Annotation[] annotations = f.getDeclaredAnnotations();
-
-            if (annotations.length == 0)
-                continue;
-            String nameAnn = annotateName(annotations[0]);
-            String type = annotateType(annotations[0]);
-
-            if (nameAnn.equals(""))
-                list.add(new Parameter(AnnotType.valueOf(type), name.toLowerCase()));
-            else
-                list.add(new Parameter(AnnotType.valueOf(type), nameAnn.toLowerCase()));
-        }
-        return list;
-    }
-
-    private String annotateName(Annotation annotation) {
-        Pattern pattern = Pattern.compile("\\(name=(.+)\\)");
-        Matcher matcher = pattern.matcher(annotation.toString());
-        if (matcher.find())
-            return matcher.group(1);
-        return "";
-    }
-
-    private String annotateType(Annotation annotation) {
-        Pattern pattern = Pattern.compile("\\.([^.]+)\\(");
-        Matcher matcher = pattern.matcher(annotation.toString());
-        if (matcher.find())
-            return matcher.group(1);
-        return "";
-    }
-
-    private String paramsStr(LinkedList<Parameter> list, AnnotType type, String div, String add) {
         StringBuilder stringBuilder = new StringBuilder();
-
-        LinkedList<Parameter> lst = new LinkedList<>(list);
-
-        ListIterator<Parameter> iterator = lst.listIterator();
-        if (type == AnnotType.Column || type == AnnotType.PrimaryKey) {
-            while (iterator.hasNext()) {
-                if (iterator.next().type != type)
-                    iterator.remove();
-            }
-        }
-
-        iterator = lst.listIterator();
-        while (iterator.hasNext()) {
-            if (type == AnnotType.Secret) {
-                iterator.next();
-                stringBuilder.append("?");
-            } else {
-                stringBuilder.append(iterator.next().name);
-            }
-            stringBuilder.append(add);
-            if (iterator.hasNext()) {
+        for (int i = 0; i < list.size(); i++) {
+            if (i != 0)
                 stringBuilder.append(div);
-            }
-
+            stringBuilder.append(list.get(i));
+            stringBuilder.append(add);
         }
-
         return stringBuilder.toString();
     }
 
